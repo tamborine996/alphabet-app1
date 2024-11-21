bbada52fa330b54137f9e75bf8265457c98ef230
tamborine996 <mqc200@gmail.com>
2024-11-21 12:26:37 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { AlphabetSpeechApp } from '../components/AlphabetSpeechApp';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <AlphabetSpeechApp />
+    </div>
+  );
+}
